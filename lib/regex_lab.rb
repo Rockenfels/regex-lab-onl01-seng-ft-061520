f25eda12528f53d527b1fd2a7df349165b55a31f
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-29 20:16:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  binding.pry
+  word.match(/[aeiou]\w*/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  test.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  test.scan(/\s\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  word[0].match(/[A-Z]\w+\./) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  word.match(/\(*(\d){3}\)*[ -]*(\d){3}[ -]*(\d){4}/) ? true: false
 end
